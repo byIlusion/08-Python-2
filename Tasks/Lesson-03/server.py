51ad02bdb26175da32ed28686c2ae33b9fbf38b1
Ilya <by_Ilusion@mail.ru>
2021-06-03 23:10:11 +0700

    Task of Lesson-03 completed

---
+++ Tasks/Lesson-03/server.py
@@ -0,0 +1,81 @@
+"""
+1. Реализовать простое клиент-серверное взаимодействие по протоколу JIM (JSON instant messaging)
+    a. клиент отправляет запрос серверу;
+    b. сервер отвечает соответствующим кодом результата.
+Клиент и сервер должны быть реализованы в виде отдельных скриптов, содержащих соответствующие функции.
+
+Функции сервера:
+    - принимает сообщение клиента;
+    - формирует ответ клиенту;
+    - отправляет ответ клиенту;
+    - имеет параметры командной строки:
+        - -p <port> — TCP-порт для работы (по умолчанию использует 7777);
+        - -a <addr> — IP-адрес для прослушивания (по умолчанию слушает все доступные адреса).
+"""
+import argparse
+import socket
+from time import time
+from socket import *
+import pickle
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Client for chat messages')
+    parser.add_argument('-a', action='store', dest='addr', default='0.0.0.0', help='Server IP-address')
+    parser.add_argument('-p', action='store', dest='port', default='7777', help='Server port')
+    args = parser.parse_args()
+    server_start(args.addr, int(args.port))
+
+
+def server_start(addr: str, port: int):
+    print(f'\n=== Init Server {addr}:{port} ===\n')
+
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind((addr, port))
+    s.listen(5)
+    s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+    s.settimeout(30)
+
+    server_listen(s)
+
+
+def server_listen(s: socket):
+    while True:
+        print('listen')
+        try:
+            client, c_addr = s.accept()
+            print(f'Connected client from {c_addr}')
+            data = client.recv(1024)
+            query = pickle.loads(data)
+            print(query)
+            if query and 'action' in query:
+                if query['action'] == 'presence':
+                    server_response(client, 200)
+                else:
+                    server_response(client, 500)
+            else:
+                server_response(client, 400)
+            client.close()
+        except Exception as e:
+            pass
+
+
+def send_message(s: socket, msg: dict):
+    s.send(pickle.dumps(msg))
+
+
+def server_response(s: socket, error_code: int):
+    errors = {
+        200: 'OK',
+        400: 'Ошибка запроса',
+        500: 'Ошибка сервера',
+    }
+    msg = {
+        'response': error_code,
+        'alert': errors[error_code]
+    }
+    send_message(s, msg)
+
+
+if __name__ == '__main__':
+    main()
